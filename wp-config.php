c78c877c6d2d465e5465382e19dd0514c9974208
lazycoder <alazycoder101@gmail.com>
2022-03-15 23:39:01 +0000

    support debug

--- wp-config.php
+++ wp-config.php
@@ -75,7 +75,9 @@ $table_prefix  = 'wp_';
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', $_ENV["WP_DEBUG"] || false);
+define('WP_DEBUG', $_ENV["WP_DEBUG"]==true || false);
+define('WP_DEBUG_LOG', $_ENV["WP_DEBUG_LOG"]=='true' || false);
+// @ini_set( 'display_errors', 0 );
 
 /* That's all, stop editing! Happy blogging. */
 
